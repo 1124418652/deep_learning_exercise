0c2e232966a0632ea8378981348f57a802d1b137
徐何君 <1124418652@qq.com>
2018-10-23 23:58:37 +0800

    modify predict function

--- machine-learning-ex3/python/EXE3.py
+++ machine-learning-ex3/python/EXE3.py
@@ -10,7 +10,7 @@
 import os
 import time
 import json
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy import io
 from collections import namedtuple
@@ -22,7 +22,7 @@ class EXE3_1(object):
 	"""
 	使用逻辑斯蒂回归实现手写数字 0-9 的识别
 	initial parameters:
-	@ data_set: simple of model training 
+	@ data_set: simple of model training
 	@ labels: the label of every element in data_set
 	@ classes: the number of labels
 	"""
@@ -74,7 +74,7 @@ class EXE3_1(object):
 
 		for i in range(1, 10):
 			labels[i] = np.where(labels[i] == i, 1, 0)
-		
+
 		for i in range(iterate):
 			da = 1 / self.num * (self.sigmod(data, self.w1_array, self.b1) - labels)
 			dw = da * data.T
@@ -127,7 +127,7 @@ class EXE3_2(EXE3_1):
 
 		return label_array
 
-	def back_prop(self, iterate = 100, alpha = 1):
+	def back_prop(self, iterate = 2000, alpha = 1):
 		"""
 		神经网络的反向传播，训练 w1_array, b1, w2_array, b2
 		# @iterate：迭代次数
@@ -136,7 +136,7 @@ class EXE3_2(EXE3_1):
 		start = time.time()
 		a0 = np.mat(self.data_set).T
 		labels = self.set_labels(self.label_num, self.num, self.labels)
-		
+
 		for i in range(iterate):
 			a1, a2 = self.feed_prop(a0)
 			dz2 = 1 / self.num * (a2 - labels)         # (dl / da2) * (da2 / dz)
@@ -145,17 +145,10 @@ class EXE3_2(EXE3_1):
 			dz1 = np.multiply(self.w2_array.T * dz2, np.multiply(a1, (1 - a1)))
 			dw1 = dz1 * a0.T
 			db1 = dz1.sum(axis = 1)
-			self.w2_array -= alpha * dw2 
+			self.w2_array -= alpha * dw2
 			self.b2 -= alpha * db2
 			self.w1_array -= alpha * dw1
 			self.b1 -= alpha * db1
-
-		# 利用一个 namedtuple 来存储训练好的网络
-		self.trained_network = namedtuple("trained_network", ["w1_array", "b1", "w2_array", "b2"])
-		self.trained_network.w1_array = self.w1_array
-		self.trained_network.b1 = self.b1
-		self.trained_network.w2_array = self.w2_array
-		self.trained_network.b2 = self.b2
 
 		EXE3_2.flag = True
 		time_used = time.time() - start
@@ -164,23 +157,34 @@ class EXE3_2(EXE3_1):
 	def dict2json(self, file_path):
 		if False == EXE3_2.flag:
 			print("Have not trained the network!")
-			return 1 
+			return 1
 
 		else:
 			if 0 != len(file_path):
 				fw = open(file_path, "w+")
-				json.dump({"w1_array": self.w1_array.tolist(), 
-					"b1": self.b1.tolist(), 
+				json.dump({"w1_array": self.w1_array.tolist(),
+					"b1": self.b1.tolist(),
 					"w2_array": self.w2_array.tolist(),
-					"b2": self.b2.tolist()}, fw)
+					"b2": self.b2.tolist(),
+					"flag": EXE3_2.flag}, fw)
 				fw.close()
 
 			else:
-				d = json.dumps({"w1_array": self.w1_array.tolist(), 
+				d = json.dumps({"w1_array": self.w1_array.tolist(),
 					"b1": self.b1.tolist(),
 					"w2_array": self.w2_array.tolist(),
-					"b2": self.b2.tolist()})
+					"b2": self.b2.tolist(),
+					"flag": EXE3_2.flag})
 				print(d)
+
+	def network_from_json(self, file_path):
+		try:
+			fr = open(file_path, "r+")
+			net_paras = json.load(fr)
+		except:
+			return 1
+
+		return net_paras
 
 	def testing(self, data, labels):
 		if False == EXE3_2.flag:
@@ -191,7 +195,7 @@ class EXE3_2(EXE3_1):
 			num = len(data)                # 测试数据的数据量
 			data = np.mat(data).T          # 将测试数据转置，以便带入模型计算
 			a1, a2 = self.feed_prop(data)
-			# a2 = a2.T 
+			# a2 = a2.T
 			res = np.zeros((len(a2), num)) # 记录测试结果
 			labels = self.set_labels(10, num, labels)     # 将测试数据的标记改为（label_num, num）的格式
 
@@ -199,24 +203,27 @@ class EXE3_2(EXE3_1):
 			for i in range(len(a2.T)):     # a2 为纵向排列的测试结果（纵向数据为分别属于该标签的概率）
 				res[np.argmax(a2.T[i])][i] = 1
 				if 1 != labels[np.argmax(a2.T[i])][i]:
-					error += 1 
-			
+					error += 1
+
 			error /= num                   # 计算错误率
 			print("The error of this network: ", error)
 
-	def predict(self, data):
-		if False == EXE3_2.flag:
-			print("You have not training the model!\nRun function back_prop() first!")
+	def predict(self, data, *argv, **kwargv):
 
-		else:
-			# print(self.w2_array, self.b1)
-			data = np.mat(data).T 
-			a1, a2 = self.feed_prop(data)
-			a2 = a2.T
+		net_paras = self.network_from_json(kwargv["file_path"])
 
-			for i in range(len(a2)):
-				print("The result of predict of picture %d is: %d" %(i, np.argmax(a2[i])))
+		self.w1_array = np.mat(net_paras["w1_array"])
+		self.w2_array = np.mat(net_paras["w2_array"])
+		self.b1 = np.mat(net_paras["b1"])
+		self.b2 = np.mat(net_paras["b2"])
+		EXE3_2.flag = net_paras["flag"]
 
+		data = np.mat(data).T
+		a1, a2 = self.feed_prop(data)
+		a2 = a2.T
+
+		for i in range(len(a2)):
+			print("The result of predict of picture %d is: %d" %(i, np.argmax(a2[i])))
 
 
 def demo1():
@@ -231,19 +238,21 @@ def demo1():
 	exe3_1.testing(data_set[1000:5000], labels[1000:5000])
 
 def demo2():
+	base_path = os.getcwd()
 	path = "../ex3/ex3data1.mat"
 	data = io.loadmat(path)
 	data_set = data["X"]
 	labels = data["y"]
 	exe3_2 = EXE3_2(data_set, labels)
 	# exe3_2.show_img()
-	exe3_2.set_warray(25, 10)
-	# exe3_2.set_labels(10, len(labels), labels)
-	exe3_2.back_prop()
+	# exe3_2.set_warray(25, 10)
+	# exe3_2.back_prop()
+
+	jsonPath = os.path.join(base_path, "trained_network.txt")
+	# exe3_2.dict2json(jsonPath)
+	exe3_2.network_from_json(jsonPath)
+	exe3_2.predict(data_set[000: 100], file_path = jsonPath)
 	exe3_2.testing(data_set[5:4000], labels[5:4000])
-	exe3_2.dict2json("trained_network.txt")
-	# exe3_2.predict(data_set[2000: 2100])
-	# exe3_2.feed_prop(data_set, labels)
 
 def main():
 	demo2()
