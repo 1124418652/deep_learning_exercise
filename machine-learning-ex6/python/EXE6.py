cad8080b41c3e452f0cced1028f2e2ce0cc3317f
hejun <1124418652@qq.com>
2018-10-27 23:57:53 +0800

    add function show_data

--- machine-learning-ex6/python/EXE6.py
+++ machine-learning-ex6/python/EXE6.py
@@ -17,15 +17,29 @@ class SVM(object):
 		pass
 
 	def show_data(self, x, y):
-		
-		
+		x_positive = []
+		x_negtive = []
+
+		for index, val in enumerate(y):
+			if 1 == val:
+				x_positive.append(x[index])
+			else:
+				x_negtive.append(x[index])
+
+		x_negtive, x_positive = np.array(x_negtive), np.array(x_positive)
+		fig = plt.figure(figsize = (6, 4))
+		plt.plot(x_positive[:, 0], x_positive[:, 1], "g+", label = "positive pointers")
+		plt.plot(x_negtive[:, 0], x_negtive[:, 1], "ro", label = "negtive pointers")
+		plt.show()
 
 def demo():
 	basePath = os.getcwd()
 	filePath = os.path.join(basePath, "../ex6/ex6data1.mat")
 	data_set = io.loadmat(filePath)
 	x, y = data_set["X"], data_set['y']
-	print(data_set)
+	# print(data_set)
+	demo1 = SVM()
+	demo1.show_data(x, y)
 
 def main():
 	demo()
