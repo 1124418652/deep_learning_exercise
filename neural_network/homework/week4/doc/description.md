f46eda76381a881ad9d9edb01aaeae6bdb29ffd6
hejun <1124418652@qq.com>
2018-11-26 22:15:56 +0800

    function back_propgation

--- neural_network/homework/week4/doc/description.md
+++ neural_network/homework/week4/doc/description.md
@@ -1,4 +1,18 @@
 @[TOC](目录)
 
 # task
-构建一个多层的神经网络，每一层分别使用 ReLU 或者 Sigmod 作为激活函数。
+构建一个多层的神经网络，每一层分别使用 ReLU 或者 Sigmod 作为激活函数。  
+要求构建的模型具有以下功能：  
+1. 能够以可以接收的错误率对未知图片进行识别
+2. 在进行模型训练时，能够绘制 cost-iteration curve, testingerror-iteration curve, step-cost-iteration curve
+
+# the structure of model
+1. 导入训练数据和测试数据
+2. 网络层数以及节点数设置
+3. 网络参数的初始化
+4. 前向传播
+ 4.1 计算一层中线性求和的部分
+ 4.2 计算激活函数的输出（ReLU或者Sigmod）
+ 4.3 结合线性求和与激活函数
+5. 计算误差
+6. 反向传播
--- neural_network/homework/week4/src/multi_layer_neural_network.py
+++ neural_network/homework/week4/src/multi_layer_neural_network.py
@@ -3,3 +3,201 @@
 import os
 import h5py
 import numpy as np 
+
+__all__ = ['Mult_layer_network']
+
+
+class Mult_layer_network(object):
+
+	def __init__(self):
+		pass
+
+	def load_data(self, filename, type = 'train'):
+		if not os.path.exists(filename):
+			raise(ValueError)
+		try:
+			with h5py.File(filename, 'r') as fr:
+				if 'train' == type.lower():
+					data_set = fr['train_set_x'][:]
+					labels = fr['train_set_y'][:]
+				elif 'test' == type.lower():
+					data_set = fr['test_set_x'][:]
+					labels = fr['test_set_y']
+			return [data_set.reshape(data_set.shape[0], -1).T, \
+					labels]
+		except:
+			print("Can't open the file!")
+			raise(ValueError)
+
+	def init_network(self, num_layer = 3, **kwargs):
+		"""
+		parameters of this function:
+		@ num_layer: int类型，表示神经网络的层数，不包括输入层
+		             num_layer == len(nodes)
+		parameters in kwargs:
+		@ feature_num: int类型，每个样本的特征数（用于初始化第一层的节点）
+		@ nodes: list类型，表示每一层网络中的节点数
+		"""
+
+		if not isinstance(num_layer, int): raise(ValueError)
+		np.random.seed(12345)
+		nodes = [20, 10, 1]
+		if 'nodes' in kwargs: nodes = kwargs['nodes']
+		assert('feature_num' in kwargs)
+		assert(len(nodes) == num_layer)
+		feature_num = kwargs['feature_num']
+
+		w_array = {}         # 存储每一层的 w
+		b_array = {}
+		w_array[1] = np.random.randn(nodes[1], feature_num) \
+				       * np.sqrt(1 / feature_num)
+		b_array[1] = np.zeros((nodes[1], 1))
+		for layer in range(2, num_layer + 1, 1):
+			w_array[layer] = np.random.randn(nodes[layer], nodes[layer - 1])\
+						   * np.sqrt(1 / nodes[layer - 1])
+			b_array[layer] = np.zeros((nodes[layer], 1))
+		return w_array, b_array
+
+	def _activate_func(self, data_set, w, b, type = "sigmod"):
+		"""
+		parameters of this function:
+		@ data_set: 输入的样本数据（每个样本占一列，每个特征占一行）
+		@ w, b: 线性部分的参数
+		@ type: 使用的激活函数的类型（'sigmod', 'relu'）
+		"""
+
+		if not isinstance(data_set, np.matrixlib.defmatrix.matrix):
+			data_set = np.mat(data_set)
+		if not isinstance(w, np.matrixlib.defmatrix.matrix):
+			w = np.mat(w)
+		if not isinstance(b, np.matrixlib.defmatrix.matrix):
+			b = np.mat(b)
+
+		assert(w.shape[1] == data_set.shape[0])
+		assert(w.shape[0] == b.shape[0])
+
+		if "sigmod" == type.lower():
+			return 1 / (1 + np.exp(-(w * data_set + b)))
+		elif "relu" == type.lower():
+			return np.maximum(0, w * data_set + b)
+
+	def _gradient_of_activate_func(self, a, type = 'sigmod'):
+		"""
+		parameters of this function:
+		@ a: 激活函数求解得到的值
+		@ type: 激活函数的类型
+		"""
+
+		if not isinstance(a, np.matrixlib.defmatrix.matrix):
+			a = np.mat(a)
+		if 'sigmod' == type.lower():
+			da = np.multiply(a, (1 - a))
+			assert(da.shape == a.shape)
+		elif 'relu' == type.lower():
+			da = np.where(a > 0, 1, 0)
+		elif 'leaky_relu' == type.lower():
+			da = np.where(a >= 0, 1, 0.01)
+		elif 'tanh' == type.lower():
+			da = 1 - np.power(a, 2)
+			assert(a.shape == da.shape)
+		return da
+
+	def forword_propgation(self, data_set, w_array, b_array, activate_func):
+		"""
+		parameters in this function:
+		@ data_set: 输入的样本数据（每个样本占一列， 每个特征占一行）
+		@ w_array: dict类型，key 表示层，value 表示对应层的 w 矩阵
+		@ b_array: dict类型，key 表示层，value 表示对应层的 b 数组
+		@ activate_func: dict类型，每个元素表示一层网络中使用的激活函数,
+						 每个元素的值取自（'sigmod', 'relu'）
+		"""
+
+		num_layer = len(b_array)
+		a_array = {}
+		a_array[0] = data_set       # a_array[0] 为输入的样本数据
+		for layer in range(1, num_layer + 1, 1): # w_array[0] 和 b_array[0] 为第一个隐藏层的参数
+			a_array[layer] = self._activate_func(a_array[layer - 1], \
+						     w_array[layer], \
+						     b_array[layer], \
+						     activate_func[layer])
+		return a_array
+
+	def back_propgation(self,
+					    data_set,
+					    labels,
+					    step = 0.2,
+					    rate_dacay = 0.0005,
+					    num_layer = 3,
+					    nodes = {1:100, 2:20, 3:1},
+					    activate_func = {1:'relu', 2:'relu', 3:'sigmod'},
+					    iteration = 200):
+		"""
+		parameters in this function:
+		@ data_set: 输入的样本
+		@ step: 更新网络的步长
+		@ num_layer: 网络中总共包含的层数，不包括输入层
+		@ nodes: dict类型，表示每一层网络中的节点数
+		@ activate_func: dict类型，表示每一层网络所使用的激活函数
+		@ iteratio: int类型，表示反向传播过程中迭代的次数
+		"""
+
+		feature_num, sample_num = data_set.shape
+		w_array, b_array = demo.init_network(num_layer,\
+											 feature_num = feature_num,\
+											 nodes = nodes)
+		cost = []
+		for i in range(iteration):
+			da = {}
+			dz = {}
+			dw = {}
+			db = {}
+			layer_iter = num_layer
+			alpha = 1 / (1 + rate_dacay * i)       # 学习率（不断衰减）
+			a_array = self.forword_propgation(data_set, w_array,
+											  b_array,
+											  activate_func)
+			cost.append(-1 / sample_num * \
+						np.sum(np.multiply((1 - labels), \
+							   np.log(1 - a_array[num_layer])) \
+							   + np.multiply(labels, \
+							   np.log(a_array[num_layer]))))       # 使用最后一层的输出 a 计算 cost
+
+			da[num_layer] = (a_array[num_layer] - labels) \
+					  / np.multiply((1 - a_array[num_layer]), a_array[num_layer]) # 计算最后一层网络的 da
+			assert(da[num_layer].shape == a_array[num_layer].shape)
+			while(1 <= layer_iter):
+				dz[layer_iter] = np.multiply(da[layer_iter],\
+									self._gradient_of_activate_func(a_array[layer_iter],\
+																	activate_func[layer_iter]))
+				dw[layer_iter] = dz[layer_iter] * a_array[layer_iter - 1].T
+				assert(dw[layer_iter].shape == w_array[layer_iter].shape)
+				db[layer_iter] = np.sum(dz[layer_iter], axis = 1)
+				assert(db[layer_iter].shape == b_array[layer_iter].shape)
+
+				w_array[layer_iter] -= 1 / sample_num * alpha * dw[layer_iter]
+				b_array[layer_iter] -= 1 / sample_num * alpha * db[layer_iter]
+				
+				if layer_iter == 1:        # 计算完第一层的 dw 和 db 之后无需再计算 da[0]，因为 a[0] 未输入层
+					break
+
+				da[layer_iter - 1] = w_array[layer_iter].T * dz[layer_iter]
+				assert[da[layer_iter - 1].shape == a_array[layer_iter - 1].shape]
+				layer_iter -= 1
+		print(cost)
+
+
+if __name__ == '__main__':
+	train_filename = os.path.join(os.getcwd(), \
+				"../datasets/train_catvnoncat.h5")	
+	test_filename = os.path.join(os.getcwd(), \
+				"../datasets/test_catvnoncat.h5")
+	demo = Mult_layer_network()
+	train_dataSet, train_labels = demo.load_data(train_filename, 'train')
+	test_dataSet, test_labels = demo.load_data(test_filename, 'test')
+	train_dataSet = train_dataSet / 255
+
+	demo.back_propgation(train_dataSet, train_labels, 
+						 step = 0.2, 
+						 num_layer = 4, 
+						 nodes = {1: 200, 2: 100, 3: 10, 4: 1},
+						 activate_func = {1:'relu', 2:'relu', 3:'sigmod', 4:'sigmod'})
